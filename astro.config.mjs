1080c5603ccd6d934ea3fdf11d126f450c5a06fc
Jluis <jorge_luis1709@hotmail.com>
2024-10-23 21:06:05 -0600

    cambiando configuracion segun entorno de ejecucion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv'
 
 import tailwind from '@astrojs/tailwind';
 
@@ -9,5 +10,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://kennoyuusha.github.io',
-  base: 'portafolio',
+  base: process.env.NODE_ENV === 'production' ? 'portafolio' : undefined,
 });
